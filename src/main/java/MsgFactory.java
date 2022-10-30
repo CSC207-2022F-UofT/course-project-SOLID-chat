5fa06b0eb8498f5c377758f81c730615ac990bb7
milkdromeda <jchen9827@gmail.com>
2022-10-30 14:53:46 -0400

    Edited MsgFactory with message ID

--- src/main/java/MsgFactory.java
+++ src/main/java/MsgFactory.java
@@ -1,9 +1,13 @@
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class MsgFactory {
-    public Message createMsg(String msgType, int chatID, int senderID, Object msgContent) {
+    public Message createMsg(String msgType, String chatID, String senderID, Object msgContent) {
         if (msgType.equalsIgnoreCase("text")) {
-            return TextMessage(chatID, senderID, (String) msgContent, LocalDateTime.now());
+            return TextMessage(chatID, senderID, (String) msgContent, LocalDateTime.now(),
+                    UUID.randomUUID().toString());
+            // ID of chat that message belongs to, ID of message's sender, message text/content, timestamp of message,
+            // ID of message
         } else {
             System.out.println("A " + msgType.toLowerCase() + " is an undefined message type for this program.");
             // TODO: implement exception
