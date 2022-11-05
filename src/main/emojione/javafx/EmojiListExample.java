63e55470d7e1af7c2eba9b8636edf1743555ce8c
Bin.W <binwcs@gmail.com>
2022-11-05 01:41:32 -0400

    .

---
+++ src/main/emojione/javafx/EmojiListExample.java
@@ -0,0 +1,31 @@
+package emojione.javafx;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class EmojiListExample extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("EmojiList.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 392, 300);
+            primaryStage.setTitle("Emojis List");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
