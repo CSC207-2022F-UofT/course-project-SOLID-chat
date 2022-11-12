33eab3836603cc0578a6dafab3df1b4be76cd3cb
madhavan7 <madhav.gopakumar@mail.utoronto.ca>
2022-11-12 18:00:30 -0500

    Almost done implementation of UserRegistrationUI and UserVerificationUI

--- src/main/java/UserRegistrationUI.java
+++ src/main/java/UserRegistrationUI.java
@@ -1,34 +1,68 @@
-import javax.swing.*;
+import junit.framework.JUnit4TestAdapter;
 
-public class UserRegistrationUI implements UserRegistrationUseCase{
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class UserRegistrationUI implements UserRegistrationUseCase, ActionListener {
     private UserDatabase database;
 
-    public UserRegistrationUI(UserDatabase database){
+    //Front end related objects
+    private JFrame RegisterFrame;
+    private JPanel RegisterPanel;
+    private JLabel usernameLabel;
+    private JLabel passwordLabel;
+    private JLabel emailLabel;
+    private JTextField usernameText;
+    private JTextField passwordText;
+    private JTextField emailText;
+    private JButton register;
+
+    public UserRegistrationUI(UserDatabase database) {
         this.database = database;
     }
     void GetUserCredentials(){
-        JFrame RegisterFrame = new JFrame();
-        RegisterFrame.setSize(1000, 1000);
+        RegisterFrame = new JFrame();
+        RegisterFrame.setSize(300, 300);
         RegisterFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel RegisterPanel = new JPanel();
+        RegisterPanel = new JPanel();
         RegisterFrame.add(RegisterPanel);
 
         //The textbox for entering the Username
         RegisterPanel.setLayout(null);
-        JLabel usernameLabel = new JLabel("Username");
+        usernameLabel = new JLabel("Username");
         usernameLabel.setBounds(10, 25, 100, 25);
+        RegisterPanel.add(usernameLabel);
 
-        JTextField usernameText = new JTextField(20);
+        usernameText = new JTextField(20);
         usernameText.setBounds(100, 20, 165, 25);
         RegisterPanel.add(usernameText);
 
         //The textbox for entering the password
-        JPasswordField passwordText = new JPasswordField();
+        passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 55, 100, 25);
+        RegisterPanel.add(passwordLabel);
+
+        passwordText = new JPasswordField();
         passwordText.setBounds(100, 50, 165, 25);
         RegisterPanel.add(passwordText);
 
         //The textbox for entering the email
-        JLabel
+        emailLabel = new JLabel("Email");
+        emailLabel.setBounds(10, 85, 100, 25);
+        RegisterPanel.add(emailLabel);
+
+        emailText = new JTextField(20);
+        emailText.setBounds(100, 80, 165, 25);
+        RegisterPanel.add(emailText);
+
+        //The Button
+        register = new JButton("Register");
+        register.setBounds(100, 110, 165, 25);
+        RegisterPanel.add(register);
+
+
 
         RegisterFrame.setVisible(true);
     }
@@ -39,6 +73,8 @@ public class UserRegistrationUI implements UserRegistrationUseCase{
         }else{
             database.createUser(username, password, email, "Basic");
             System.out.println("Your account has been created, please verify to login");
+            UserVerificationUI verifyUser = new UserVerificationUI(389);
+            verifyUser.verify(email);
         }
     }
 
@@ -46,5 +82,14 @@ public class UserRegistrationUI implements UserRegistrationUseCase{
         UserDatabase testDB = new UserDatabase();
         UserRegistrationUI testUI = new UserRegistrationUI(testDB);
         testUI.GetUserCredentials();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String username = usernameText.getText();
+        String password = passwordText.getText();
+        String email = emailText.getText();
+        this.registerUser(username, password, email);
+
     }
 }
--- src/main/java/UserVerificationUI.java
+++ src/main/java/UserVerificationUI.java
@@ -1,3 +1,60 @@
-public class UserVerificationUI implements UserVerifier{
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+import java.util.Properties;
+public class UserVerificationUI implements UserVerifier, ActionListener {
+    Random random;
+    private int code;
+    private JFrame verificationFrame;
+    private JPanel verificationPanel;
+    private JLabel verificationLabel;
+    private JTextField verificationCodeText;
+    private JButton verifyButton;
+    public UserVerificationUI(int code){
+        this.code = code;
+    }
+    public void verify(String email){
+        //Creating the UI to input the verification code
+        verificationFrame = new JFrame();
+        verificationFrame.setSize(400, 200);
+        verificationFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        verificationPanel = new JPanel();
+        verificationFrame.add(verificationPanel);
 
+        verificationPanel.setLayout(null);
+        verificationLabel = new JLabel("Verification Code");
+        verificationLabel.setBounds(10,25, 200, 25);
+        verificationCodeText = new JTextField();
+        verificationCodeText.setBounds(125, 20, 165, 25);
+
+        verificationPanel.add(verificationLabel);
+        verificationPanel.add(verificationCodeText);
+
+        verifyButton = new JButton("verify");
+        verifyButton.setBounds(125, 50, 100, 25);
+        verificationPanel.add(verifyButton);
+        verifyButton.addActionListener(this);
+        verificationFrame.setVisible(true);
+    }
+
+    public static void sendVerificationCode(String email){
+        //When this is implemented, a verification code(this.code) will be sent to email with email address "email",
+        //The code will be a random number that is generated, when the user presses the register
+        // button(see UserRegistrationUI).
+    }
+    public static void main(String[] args){
+        UserVerificationUI ver = new UserVerificationUI(389);
+        ver.verify("abc");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        int verCode = Integer.parseInt(verificationCodeText.getText());
+        if(code == verCode){
+            System.out.println("verified");
+        }else{
+            System.out.println("verification unsuccessful");
+        }
+    }
 }
