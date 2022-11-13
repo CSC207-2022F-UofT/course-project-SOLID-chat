5c4b5c9642bad8f3f5e84b04f11517762374e05a
madhavan7 <madhav.gopakumar@mail.utoronto.ca>
2022-11-12 21:46:19 -0500

    added changes made to another branch and resolved conflicts

--- src/main/java/UserDatabase.java
+++ src/main/java/UserDatabase.java
@@ -3,55 +3,23 @@ import java.util.ArrayList;
 import java.util.List;
 public class UserDatabase implements UserExists, UserRetriever, UserCreator, IRetrieveList{
     File accounts;
-<<<<<<< HEAD
+    List<User> accountList;
     public UserDatabase(){
         this.accounts = new File("UserAccounts.csv");
+        this.accountList = this.getList();
     }
-    public UserDatabase(File accounts){
-        this.accounts = accounts;
-    }
-    @Override
-    public boolean UserExists(String username, String email) {
-        User user = null;
-=======
-    List<User> accountList;
     public UserDatabase(File accounts){
         this.accounts = accounts;
         this.accountList = this.getList();
     }
     @Override
     public boolean UserExists(String username, String email) {
-        /*User user = null;
->>>>>>> 15-userdatabase-and-user
-        try(FileInputStream fileIn = new FileInputStream(accounts);
-        ObjectInputStream in = new ObjectInputStream(fileIn)){
-            do{
-                try{
-                    user = (User)in.readObject();
-                }catch(NullPointerException e){
-<<<<<<< HEAD
-=======
-                    user = null;
->>>>>>> 15-userdatabase-and-user
-                    break;
-                }
-            }while(!user.getEmail().equals(email) && !user.getUsername().equals(username));
-        }catch(IOException e){
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-<<<<<<< HEAD
-        return user != null;
-=======
-        return user != null;*/
         for(User user: this.accountList){
             if(user.getUsername().equals(username) || user.getEmail().equals(email)){
                 return true;
             }
         }
         return false;
->>>>>>> 15-userdatabase-and-user
     }
 
     // Creates a new user with a username and password, and an email address
@@ -63,10 +31,7 @@ public class UserDatabase implements UserExists, UserRetriever, UserCreator, IRe
         try(FileOutputStream fileOut = new FileOutputStream(accounts)){
             try(ObjectOutputStream out = new ObjectOutputStream(fileOut)){
                 out.writeObject(newUser);
-<<<<<<< HEAD
-=======
                 this.accountList.add(newUser);
->>>>>>> 15-userdatabase-and-user
                 out.close();
                 fileOut.close();
             }catch(Exception e){
@@ -95,12 +60,6 @@ public class UserDatabase implements UserExists, UserRetriever, UserCreator, IRe
         List<User> users = new ArrayList<>();
         try(FileInputStream fileIn = new FileInputStream(accounts);
             ObjectInputStream in = new ObjectInputStream(fileIn)){
-<<<<<<< HEAD
-            User user = (User) in.readObject();
-            while(user != null){
-                users.add(user);
-                user = (User)in.readObject();
-=======
             /*User user = (User) in.readObject();*/
             while(true){
                 try{
@@ -109,7 +68,6 @@ public class UserDatabase implements UserExists, UserRetriever, UserCreator, IRe
                 catch(EOFException e){
                     break;
                 }
->>>>>>> 15-userdatabase-and-user
             }
         }catch(Exception e){
             e.printStackTrace();
