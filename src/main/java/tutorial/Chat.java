4d0cf85a9df657f4e9ad9d62dc6574082edc7afa
emma0925 <qsls2016@gmail.com>
2022-11-16 00:21:18 -0500

    'implemented search by word, time and newest'

---
+++ src/main/java/tutorial/Chat.java
@@ -0,0 +1,55 @@
+package tutorial;
+import java.util.ArrayList;
+
+/**
+ * Copied from Branch Chat Initiation
+ **/
+public class Chat {
+    protected String name;
+    protected String chatID;
+    protected String senderUsername;
+
+    // chat's conversation history
+    protected ArrayList<Message> convHist;
+
+
+    /**
+     * Get the chat's name
+     * @return name
+     */
+    public String getName(){
+        return this.name;
+    }
+
+    /**
+     * Get the chat's ID
+     * @return chatID
+     */
+    public String getChatID(){
+        return this.chatID;
+    }
+
+    /**
+     * Get the sender's username
+     * @return senderUsername
+     */
+    public String getSenderUsername(){
+        return this.senderUsername;
+    }
+
+    /**
+     * Return this conversation history of the chat
+     * @return convHist
+     */
+    public ArrayList<Message> getConvHist(){
+        return new ArrayList<Message>(this.convHist);
+    }
+
+    /**
+     * Add a message to the chat's conversation history when a message is sent or received
+     * @param message Message that is sent or received
+     */
+    public void addtoconvHist(Message message){
+        this.convHist.add(message);
+    }
+}
