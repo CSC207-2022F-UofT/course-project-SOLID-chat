632d8ef197575b787946c4eee7e7832e4fc8ffed
Parmis Mehdiyar <parmis.mehdiyar@mail.utoronto.ca>
2022-11-17 18:12:12 -0500

    Fixed feature test

--- src/test/java/TestUserSearch.java
+++ src/test/java/TestUserSearch.java
@@ -6,10 +6,9 @@ import java.io.File;
 public class TestUserSearch {
     @Test
     public void TestShowUser() {
-        File accounts = new File("TestUserDatabase2.csv");
-        UserDatabase accountDatabase = new UserDatabase(accounts);
+        UserDatabase db = new UserDatabase();
         UserSearchUI ui = new UserSearchUI();
-        accountDatabase.createUser("parmism", "123", "parmis@gmail.com", "Basic");
+        db.createUser("parmism", "123", "parmis@gmail.com", "Basic");
         String output = "<html>Username: parmism<br>Email: parmis@gmail.com</html>";
         Assertions.assertEquals((ui.showProfile("parmism")), (output));
     }
