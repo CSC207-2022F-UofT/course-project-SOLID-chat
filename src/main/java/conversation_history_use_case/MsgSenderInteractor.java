33d6c52b6de52175a406ecf8c5210a1816e0d8f2
milkdromeda <jchen9827@gmail.com>
2022-11-19 13:22:08 -0500

    Separated interactor into two interactors to adhere to SRP

--- src/main/java/conversation_history_use_case/MsgSenderInteractor.java
+++ src/main/java/conversation_history_use_case/MsgSenderInteractor.java
@@ -1,17 +1,17 @@
 package conversation_history_use_case;
 
-import entities.*;
-
+import entities.Message;
+import entities.MsgFactory;
 import temp_persistence.UserDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Interactor responsible for displaying this history upon opening a chat
+ * Interactor responsible for adding messages to a chat's conversation history
  */
 //public class ConvHistInteractor implements ConversationHistoryInputBoundary{
-public class ConvHistInteractor{
+public class MsgSenderInteractor {
     /**
      * File and in-memory storage of users and their chats (incl. conversation history)
      */
@@ -32,26 +32,30 @@ public class ConvHistInteractor{
      * //@param convHistPresenter presenter
      */
 //    public ConvHistInteractor(UserDatabase userDatabase, MsgFactory msgFactory, ConvHistPresenter convHistPresenter) {
-    public ConvHistInteractor(UserDatabase userDatabase, MsgFactory msgFactory) {
+    public MsgSenderInteractor(UserDatabase userDatabase, MsgFactory msgFactory) {
         this.userDatabase = userDatabase;
         this.msgFactory = msgFactory;  // msgType of MsgFactory specified in Main
 //        this.convHistPresenter = convHistPresenter;
     }
 
     /**
-     * Displays conversation history upon opening a chat
+     * Creates and adds message to a chat's conversation history
      * @param requestModel input data
      * @return a response model for presenter
      */
 //    @Override
-    public ConvHistResponseModel create(ConvHistRequestModel requestModel) {
-        String userID = requestModel.getUserID();
+    public ConvHistResponseModel create(MsgSenderRequestModel requestModel) {
+        // Create new message
+        Message message = msgFactory.createMsg(requestModel.getSenderID(), requestModel.getMsgContent());
+
+        // Add message to specified chat in user list
+        String userID = requestModel.getSenderID();  // need a user in the chat to get the chat
         String chatID = requestModel.getChatID();
 
-        ConvHistDsRequestModel dsRequestModel = new ConvHistDsRequestModel(userID, chatID);
+        MsgSenderDsRequestModel dsRequestModel = new MsgSenderDsRequestModel(userID, chatID, message);
 
         // Access database (code for database will become functional after PR for issue 15 is merged)
-//        List<Message> conversationHistory = userDatabase.getConversationHistory(dsRequestModel);
+//        List<Message> conversationHistory  = userDatabase.saveMessage(dsRequestModel);
 
         // Presenter show success view (code to be written); below is temporary
         List<Message> conversationHistory = new ArrayList<>();
