65b99724dfe65373ba0e033b6d2ea8c329f0aa5c
Parmis Mehdiyar <parmis.mehdiyar@mail.utoronto.ca>
2022-11-20 14:13:07 -0500

    Revert "Delete TestUserChange.java"
    This reverts commit 8860f1e31d1bfcba85f612e5d5b81542f8625989.

---
+++ src/test/java/TestUserChange.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import screens.profile_update_screen.UserModificationUI;
+
+public class TestUserChange {
+    public void correctChange() {
+        UserDatabase db = new UserDatabase();
+        db.createUser("parmism", "123", "parmis@gmail.com", "Basic");
+        UserModificationUI ui = new UserModificationUI();
+        ui.reportChange("parmism", "123", "Email", "parmis@yahoo.com");
+        UserDatabase db2 = new UserDatabase();
+        String actual = db2.getUser("parmism").getEmail();
+        Assertions.assertEquals("parmis@yahoo.com", actual);
+    }
+}
