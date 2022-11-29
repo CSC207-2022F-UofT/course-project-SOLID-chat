a6d0b75fb6e322edec2bf4969eeacafe19441e17
madhavan7 <madhav.gopakumar@mail.utoronto.ca>
2022-11-29 15:18:01 -0500

    Merged conflicts from the main branch

--- src/main/java/Entities/user_entities/BasicUser.java
+++
@@ -1,7 +0,0 @@
-package Entities.user_entities;
-
-public class BasicUser extends User {
-    public BasicUser(String Username, String Password, String Email){
-        super(Username, Password, Email);
-    }
-}
--- src/main/java/Entities/user_entities/UserFactory.java
+++
@@ -1,8 +0,0 @@
-package Entities.user_entities;
-
-public class UserFactory {
-    //Following the factory design pattern, just in case in the future we decide to add various different types of Users
-    public static User BirthUser(String Username, String Password, String Email, String type){
-        return new BasicUser(Username, Password, Email);
-    }
-}
