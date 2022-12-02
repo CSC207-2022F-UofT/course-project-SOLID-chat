b44d2fff6c075489aaed6c4b4253047706a7bb37
Nasim Bondar Sahebi <sogol.sahebi@mail.utoronto.ca>
2022-12-02 17:41:53 -0500

    added documentations fot chat-initiation part

--- src/main/java/entities/chat/CommonPrivatechat.java
+++ src/main/java/entities/chat/CommonPrivatechat.java
@@ -1,5 +1,11 @@
 package entities.chat;
 
+// Entity layer.
+/**
+ * This class  implements teh PrivateChatFactory Interfaces and Create a new obj of a privatechat
+ * given their name, chatID and recipient Username
+ */
+
 public class CommonPrivatechat implements PrivateChatFactory{
 
     @Override
