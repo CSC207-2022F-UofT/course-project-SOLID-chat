8658b2954e161a1f2fd11d39916b3abe2a38739b
Amy <amy.yong11@yahoo.com>
2022-12-04 11:36:42 -0500

    Added tester file

--- src/test/java/TestAppScreen.java
+++ src/test/java/TestAppScreen.java
@@ -1,31 +1,36 @@
 import data_access.UserDatabase;
-import entities.chat.Chat;
-import entities.user_entities.User;
-import interface_adapters.chat.UserChats;
-
 import java.io.File;
 
+// this is just for testing the UI
 public class TestAppScreen {
 
     public static void main(String[] args) {
 
-        UserDatabase userDatabase = new UserDatabase(new File("user_accounts"));
-        System.out.println(userDatabase.UserExists("amy"));
-        User amy = userDatabase.getUser("amy");
-        amy.login();
+        UserDatabase userDatabase = new UserDatabase(new File("tester_file"));
+
+//          List<User> users = userDatabase.getList();
+//          for (User user: users){
+//              System.out.println(user.getUsername());
+//          }
+
+//        System.out.println(userDatabase.UserExists("amy"));
+//        User amy = userDatabase.getUser("amy");
+//        amy.login();
+//
+//
+//        for (Chat chat: userDatabase.getUserChats("amy")){
+//            System.out.println(chat.getName());
+//        }
+//        UserChats userChats = new UserChats("amy");
+//        //userChats.getUserChats("amy").get(1);
+//        int i = 0;
+//        for (Chat chat: userChats.getUserChats("amy")){
+//            System.out.println(i);
+//            i++;
+//        }
+//        System.out.println(i);
 
 
-        for (Chat chat: userDatabase.getUserChats("amy")){
-            System.out.println(chat.getName());
-        }
-        UserChats userChats = new UserChats("amy");
-        //userChats.getUserChats("amy").get(1);
-        int i = 0;
-        for (Chat chat: userChats.getUserChats("amy")){
-            System.out.println(i);
-            i++;
-        }
-        System.out.println(i);
 
 
 
