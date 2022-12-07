5621bef78d723b3866e8f59c0bab58be6ab4db89
Nasim Bondar Sahebi <sogol.sahebi@mail.utoronto.ca>
2022-12-07 11:36:52 -0500

    chatinitiation part completed

--- src/main/java/screens/chat_screen/ChatController.java
+++ src/main/java/screens/chat_screen/ChatController.java
@@ -1,4 +1,6 @@
 package screens.chat_screen;
+import data_access.UserDatabase;
+import entities.chat.Chat;
 import entities.chat.PrivateChat;
 import interface_adapters.appscreen.AppScreenChatProxy;
 import use_cases.chat_initiation_use_case.ChatInputBoundry;
@@ -49,15 +51,33 @@ public class ChatController {
     // this method is used in UI to set the recipient username
     public ChatModel create (String username){
 
-
     ChatModel chatmodel = new ChatModel(username);
-    this.newprivatechat = chatinputboundry.create(chatmodel);
-    return chatmodel;
 
-    }
+    UserDatabase db = new UserDatabase();
+
+    //check whether is typed username exist in our database  if yes create a new privatechat
+
+    if(db.UserExists(username)) {
+       // check if the current user already has a chat with user
+
+        for (Chat chat : db.getUserChats(currentusername)) {
+            if (chat.getName().equals(username)) {
+                return null;
+            }
+        }
+
+        //create a private chat obj
+
+        this.newprivatechat = chatinputboundry.create(chatmodel);
+
+        //adding the chat to dashboard App-screen and then update in data base
+        new AppScreenChatProxy(currentusername,this.newprivatechat).proxyChat();
+        return chatmodel;
+
+    }return null;}
 
     /**
-     * a getter for the new made private chat.( will be used in testing )
+     * a getter for the new made private chat.( might be used in testing )
      * @return  PrivateChat
      */
 
@@ -65,14 +85,6 @@ public class ChatController {
         return newprivatechat;
     }
 
-    /*
-     * Adding the chat to dashboard App-screen and then update it in database
-     */
-
-
-    public void addto_appscreen(){
-        new AppScreenChatProxy(currentusername, this.newprivatechat).proxyChat();
-    }
 
 
 }
